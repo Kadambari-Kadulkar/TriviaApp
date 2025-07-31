254e8ff06bbcb9244e55eaa56013245c2fa22899
Kadambari Kadulkar <kady11395@gmail.com>
2025-07-31 17:44:42 +0100

    Initial commit in quiz-screen branch

--- app/src/main/java/com/example/triviaapp/component/QuizCompletion.kt
+++ app/src/main/java/com/example/triviaapp/component/QuizCompletion.kt
@@ -1,6 +1,8 @@
 package com.example.triviaapp.component
 
+import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 
-class QuizCompletion(navController: NavHostController, score: Int, total: Int) {
+@Composable
+fun QuizCompletion(navController: NavHostController, score: Int, total: Int) {
 }
